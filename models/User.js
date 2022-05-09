2a3e3180903126b3ee0d4ad02f7399e9715f1aaf
Jaden Storey <jadenstorey@hotmail.com>
2022-05-09 13:56:21 -0700

    created models and model index

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require("bcrypt")
+
+class User extends Model {}
+
+User.init({
+    username: {
+         type: DataTypes.STRING,
+         allowNull:false,
+         unique:true,
+    },
+    // email: {
+    //     type: DataTypes.STRING,
+    //     allowNull: false,
+    //     unique: true,
+    //     validate: {
+    //         isEmail: true
+    //     }
+    // },
+    password:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate:{
+            len:[8, 128]
+        }
+    }
+},{
+    hooks:{
+        beforeCreate:async userdata=>{
+            userdata.password = await bcrypt.hash(userdata.password,5)
+            return userdata
+        }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+});
+
+module.exports=User
