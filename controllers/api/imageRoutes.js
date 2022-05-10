ad5b8419d801552c1c91625733359b5b3fbc766c
Eli Wood <mreliwood1@gmail.com>
2022-05-10 02:02:41 -0700

    adding profile page

--- controllers/api/imageRoutes.js
+++ controllers/api/imageRoutes.js
@@ -12,17 +12,34 @@ cloudinary.config({
     cloud_name: 'delw6elgw',
     api_key: '331111644569714',
     api_secret: API_SECRET
-})
+});
 
 router.post("/upload", (req, res) => {
     const file = req.files.photo;
-    console.log(file)
+    console.log(file);
     cloudinary.uploader.upload(file.tempFilePath, (err, res) => {
-        console.log("error", err)
-        console.log("result", res)
+        console.log("error", err);
+        console.log("result", res);
         res.send({
             success:true,
             res
+        });
+    });
+});
+
+// DELETE a blog
+router.delete("/:id", (req, res) => {
+
+    cloudinary.uploader.destroy({
+        where: {
+            id: req.params.id
+        }
         })
-    })
-})
+        .then(deletedBlog => {
+            res.json(deletedBlog);
+        })
+        .catch(err => {
+            res.status(500).json({ msg: "An error occured!", err });
+        });
+
+});
