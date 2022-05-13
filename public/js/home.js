dae73e536545ce075e325dcbe593d86a22081282
Eli Wood <mreliwood1@gmail.com>
2022-05-13 12:16:08 -0700

    styling update

--- public/js/home.js
+++ public/js/home.js
@@ -1,1 +1,15 @@
-// TODO: add event listeners and handlers
+var slideIndex = 0;
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
+carousel();
