ea58102ff567a65c3f66b098abb76891a2641663
Eli Wood <mreliwood1@gmail.com>
2022-05-13 14:09:42 -0700

    updated home page styling

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,25 @@
-var slideIndex = 0;
+// automatic slideshow
+let slideIndex = 0;
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
+
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1} 
-  x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 3000); // Change image every 2 seconds
+
+  if (slideIndex > slides.length) {slideIndex = 1}
+
+  if (slideIndex === 1) {
+    slides[slides.length - 1].style.opacity = "0";
+    slides[0].style.opacity = "1";
+  } else {
+    slides[slideIndex - 2].style.opacity = "0";
+    slides[slideIndex - 1].style.opacity = "1";
+  }
+
+  setTimeout(carousel, 4000); // Change image every 4 seconds
+
 }
 
 carousel();
